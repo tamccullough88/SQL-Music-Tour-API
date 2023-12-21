1d43801fce559a5049e1e334849f69ca9d521eed
tamccullough88 <tamccullough88@gmail.com>
2023-12-20 19:04:24 -0500

    added port and sequelize connection

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+//Sequelize Connection
+
+const sequelize = new Sequelize(process.env.PG_URI)
+
+
+try {
+    sequelize.authenticate()
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+} catch (err) {
+    console.log(`Unable to connect to PG: ${err}`)
+}
+
+
+
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
